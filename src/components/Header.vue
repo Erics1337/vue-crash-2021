374d0de52bac4730d672a563f5079b2991055d91
Eric <erics1337@gmail.com>
2022-06-21 17:04:54 -0600

    Full CRUD Functionality w/ Static Data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
 	<header>
 		<h1>{{ title }}</h1>
-		<Button text="Add Task" color="Green" />
+		<Button
+			@btn-click="$emit('toggle-add-task')"
+			:text="showAddTask ? 'Close' : 'Add Task'"
+			:color="showAddTask ? 'red' : 'green'"
+		/>
 	</header>
 </template>
 
@@ -10,6 +14,7 @@ import Button from './Button'
 export default {
 	name: 'Header',
 	props: {
+		showAddTask: Boolean,
 		title: {
 			type: String,
 			default: 'Hello World',
